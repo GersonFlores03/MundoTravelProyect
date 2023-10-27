95cdc63a702785e93ba4e7d94175525199c8f5ce
Nelson Juarez <Nelson Juarez@LAPTOP-0D3S45V2>
2023-10-26 23:29:36 -0500

    JIJIJIJA

--- src/mundotravel/Categoria.java
+++ src/mundotravel/Categoria.java
@@ -4,7 +4,7 @@ package mundotravel;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-// GENTEEEEEEEEEEEEEEEEE
+// CUALQUIER COSA JIJIJIJA
 /**
  *
  * @author USER
